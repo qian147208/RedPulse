d92b6545219866c76967a2b0ca97bbc5407f3b9b
qianchaoduo <1445007455@qq.com>
2026-05-15 22:15:07 +0800

    Initial Commit

---
+++ RedbookRefill/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RedbookRefill
+//
+//  Created by mac on 2026/5/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RedbookRefill/RedbookRefillApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedbookRefillApp.swift
+//  RedbookRefill
+//
+//  Created by mac on 2026/5/15.
+//
+
+import SwiftUI
+
+@main
+struct RedbookRefillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
